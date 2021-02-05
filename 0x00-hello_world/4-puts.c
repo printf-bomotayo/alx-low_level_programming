236e1300e981da10847186459c533448456ee592
printf-bomotayo <bomotayo2020@outlook.com>
2021-02-05 04:08:17 +0000

    writing a c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - main block
+ * main - main c block where the codes are executed
  * Return: 0
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+	return (0);
 }
